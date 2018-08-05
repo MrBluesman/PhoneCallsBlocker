948285f7370f2bf92b405b95c340212b3b781863
Łukasz <l.parysek@gmail.com>
2018-08-05 22:03:57 +0200

    Fragmeny dla rejestru blokowań

--- app/src/main/java/com/example/ukasz/phonecallsblocker/StartActivity.java
+++ app/src/main/java/com/example/ukasz/phonecallsblocker/StartActivity.java
@@ -348,47 +348,47 @@ public class StartActivity extends AppCompatActivity implements SettingsFragment
         return autoBlockEnabled;
     }
 
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment
-    {
-        /**
-         * The fragment argument representing the section number for this.
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
-
-        public PlaceholderFragment()
-        {
-
-        }
-
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber)
-        {
-            Log.e("sectionNumber:", String.valueOf(sectionNumber));
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState)
-        {
-            View rootView = inflater.inflate(R.layout.fragment_start, container, false);
-            TextView textView = rootView.findViewById(R.id.section_label);
-            textView.setText(getString(R.string.start_activity_section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
-            return rootView;
-        }
-    }
+//
+//    /**
+//     * A placeholder fragment containing a simple view.
+//     */
+//    public static class PlaceholderFragment extends Fragment
+//    {
+//        /**
+//         * The fragment argument representing the section number for this.
+//         * fragment.
+//         */
+//        private static final String ARG_SECTION_NUMBER = "section_number";
+//
+//        public PlaceholderFragment()
+//        {
+//
+//        }
+//
+//        /**
+//         * Returns a new instance of this fragment for the given section
+//         * number.
+//         */
+//        public static PlaceholderFragment newInstance(int sectionNumber)
+//        {
+//            Log.e("sectionNumber:", String.valueOf(sectionNumber));
+//            PlaceholderFragment fragment = new PlaceholderFragment();
+//            Bundle args = new Bundle();
+//            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+//            fragment.setArguments(args);
+//            return fragment;
+//        }
+//
+//        @Override
+//        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+//                                 Bundle savedInstanceState)
+//        {
+//            View rootView = inflater.inflate(R.layout.fragment_start, container, false);
+//            TextView textView = rootView.findViewById(R.id.section_label);
+//            textView.setText(getString(R.string.start_activity_section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
+//            return rootView;
+//        }
+//    }
 
     /**
      * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
@@ -421,14 +421,13 @@ public class StartActivity extends AppCompatActivity implements SettingsFragment
             switch (position)
             {
                 case 0:
-                    return PlaceholderFragment.newInstance(position + 1);
+                    return RegistryFragment.newInstance(1);
                 case 1:
                     return PhoneBlockFragment.newInstance(1);
                 case 2:
                     return SettingsFragment.newInstance();
-                default:
-                    return PlaceholderFragment.newInstance(position + 1);
             }
+            return null;
         }
 
         /**
