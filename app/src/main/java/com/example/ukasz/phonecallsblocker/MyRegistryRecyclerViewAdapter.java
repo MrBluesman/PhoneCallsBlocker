f9069adcf1697b1933640d1a2e9a3bbee14bfe4e
Łukasz <l.parysek@gmail.com>
2018-08-30 20:58:10 +0200

    Layout dla itemu w rejestrze blokowań zmieniony na RelativeLayout

--- app/src/main/java/com/example/ukasz/phonecallsblocker/MyRegistryRecyclerViewAdapter.java
+++ app/src/main/java/com/example/ukasz/phonecallsblocker/MyRegistryRecyclerViewAdapter.java
@@ -4,8 +4,11 @@ import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.PopupMenu;
 import android.widget.TextView;
 
 import com.example.ukasz.androidsqlite.RegistryBlock;
@@ -32,8 +35,8 @@ public class MyRegistryRecyclerViewAdapter extends RecyclerView.Adapter<MyRegist
         final View mViewContainer;
         //        final TextView mIdView;
         final TextView mNrRegisteredBlock;
-        final TextView mNrDate;
-
+        final TextView mDate;
+        final ImageView mItemOptions;
 
 
         /**
@@ -47,7 +50,8 @@ public class MyRegistryRecyclerViewAdapter extends RecyclerView.Adapter<MyRegist
             mViewContainer = view;
 //            mIdView = (TextView) view.findViewById(R.id.item_number);
             mNrRegisteredBlock = view.findViewById(R.id.registry_item_phone_number);
-            mNrDate = view.findViewById(R.id.registry_item_date);
+            mDate = view.findViewById(R.id.registry_item_date);
+            mItemOptions = view.findViewById(R.id.registry_item_options);
         }
 
 
@@ -105,7 +109,44 @@ public class MyRegistryRecyclerViewAdapter extends RecyclerView.Adapter<MyRegist
     {
         RegistryBlock rBlock = mRegistryBlockings.get(position);
         holder.mNrRegisteredBlock.setText(rBlock.getNrBlocked());
-        holder.mNrDate.setText(rBlock.getNrBlockingDate().toString());
+        holder.mDate.setText(rBlock.getNrBlockingDate().toString());
+
+        //show item menu
+        holder.mItemOptions.setOnClickListener(new View.OnClickListener()
+        {
+
+            @Override
+            public void onClick(View v)
+            {
+                //creating a popup menu
+                PopupMenu popup = new PopupMenu(mContext, holder.mItemOptions);
+                //inflating menu from xml resource
+                popup.inflate(R.menu.menu_registry_item);
+                //adding click listener
+                popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener()
+                {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item)
+                    {
+                        switch (item.getItemId())
+                        {
+                            case R.id.menu_action_details:
+                                //handle details click
+                                break;
+                            case R.id.menu_action_delete:
+                                //handle delete click
+                                break;
+                            case R.id.menu_action_delete_all_related:
+                                //handle delete all related click
+                                break;
+                        }
+                        return false;
+                    }
+                });
+                //displaying the popup
+                popup.show();
+            }
+        });
     }
 
     @Override
