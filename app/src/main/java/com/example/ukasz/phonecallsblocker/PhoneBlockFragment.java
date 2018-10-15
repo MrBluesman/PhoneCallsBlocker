a916d66158ae5de43f71266e5d209bfcb0300bcf
Łukasz <l.parysek@gmail.com>
2018-10-15 23:44:06 +0200

    Fix dodawania tylko gdy blokowanie jeszcze nie istnieje

--- app/src/main/java/com/example/ukasz/phonecallsblocker/PhoneBlockFragment.java
+++ app/src/main/java/com/example/ukasz/phonecallsblocker/PhoneBlockFragment.java
@@ -157,10 +157,10 @@ public class PhoneBlockFragment extends Fragment implements SwipeRefreshLayout.O
         swipeRefreshLayout.setOnRefreshListener(this);
 
         //Firebase realtime database references
-        //TODO: initialize persistence setting as soon as possible (after app start)
-        FirebaseDatabase.getInstance().setPersistenceEnabled(true);
+        //TODO: initialize keep Synced when is really needed and remember to unSync it
         databaseRef = FirebaseDatabase.getInstance().getReference();
         blockingsRef = databaseRef.child("blockings");
+        blockingsRef.keepSynced(true);
 
         //Set the adapter
         if (view instanceof RecyclerView)
